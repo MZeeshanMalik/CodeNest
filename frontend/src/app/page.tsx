dafd4dc826f76ba2e5b9f402f4970067eed7a423
M zeeshan <m121zeeshan@gmail.com>
2025-11-01 12:01:04 +0500

    updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,37 @@
-'use client";';
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { HeorSection } from "@/components/Home/HeorSection";
 import CategoriesSection from "@/components/Home/CategoriesSection";
 import FeatureSection from "@/components/Home/FeatureSection";
 import TestimonialSlider from "@/components/Home/Testimonial";
 import TopContribution from "@/components/Home/TopContribution";
 import WhyJoinUs from "@/components/Home/WhyJoinUs";
-import { FaQuestionCircle, FaReply, FaUsers } from "react-icons/fa";
-import { MdOutlineHowToVote } from "react-icons/md";
-import { SlBadge } from "react-icons/sl";
 
 function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+    const userParam = searchParams.get("user");
+
+    if (token && userParam) {
+      try {
+        const user = JSON.parse(decodeURIComponent(userParam));
+
+        // Save to localStorage
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+
+        // Remove query params (for cleaner URL)
+        router.replace("/"); // or router.replace("/") if this is your main page
+      } catch (err) {
+        console.error("Failed to parse user:", err);
+      }
+    }
+  }, [searchParams, router]);
+
   return (
     <div className="bg-gray-50">
       <HeorSection />
