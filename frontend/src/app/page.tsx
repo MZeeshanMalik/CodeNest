71c037e7efee5fe1f0035307f4693ac6725aee7f
M zeeshan <m121zeeshan@gmail.com>
2025-11-01 12:29:41 +0500

    updadted

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import React, { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { HeorSection } from "@/components/Home/HeorSection";
